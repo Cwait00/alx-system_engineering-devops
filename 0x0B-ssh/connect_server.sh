20d1ff5d896379284124ad8f8610cecb63c98b1f
Cwait00 <cwaitanobongoza@gmail.com>
2024-01-22 03:30:37 -0800

    updated

--- 0x0B-ssh/connect_server.sh
+++ 0x0B-ssh/connect_server.sh
@@ -7,5 +7,5 @@ eval $(ssh-agent)
 # Path to identityFile
 ssh-add ~/.ssh/school
 
-# Username and IP Address 
-ssh ubuntu@172.17.0.6
+# Username and IP Address
+ssh -i ~/.ssh/school ubuntu@172.17.0.6

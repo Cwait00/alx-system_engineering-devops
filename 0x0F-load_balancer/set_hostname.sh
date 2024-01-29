e4d92a9c96225c5499a141d7a55e769cbc1277a9
Cwait00 <cwaitanobongoza@gmail.com>
2024-01-29 08:55:35 -0800

    updated

--- 0x0F-load_balancer/set_hostname.sh
+++ 0x0F-load_balancer/set_hostname.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+# Set the hostname for [STUDENT_ID]-web-01
 echo "[STUDENT_ID]-web-01" | sudo tee /etc/hostname
+
+# Add the hostname to /etc/hosts
 sudo sed -i '/127.0.0.1/s/$/ [STUDENT_ID]-web-01/' /etc/hosts
+
+# Set the hostname using hostnamectl
 sudo hostnamectl set-hostname [STUDENT_ID]-web-01

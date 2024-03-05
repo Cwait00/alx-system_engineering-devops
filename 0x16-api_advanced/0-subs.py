fd58d077a2ab5ae9d6eecb45724498bce648effc
cwaita <cwaitanobongoza@gmail.com>
2024-03-05 05:51:10 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,30 +2,13 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers of the subreddit.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
-
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
-
-
-# Test the function
-if __name__ == '__main__':
-    subreddit = input("Enter the name of the subreddit: ")
-    subscribers_count = number_of_subscribers(subreddit)
-    print(f"Number of subscribers in '{subreddit}': {subscribers_count}")

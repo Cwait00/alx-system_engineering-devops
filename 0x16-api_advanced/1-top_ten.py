c96abaf3c5fab87ea6c7bf91764e3861dcf5445b
cwaita <cwaitanobongoza@gmail.com>
2024-03-05 06:52:38 -0800

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 
+"""
+Function that queries the Reddit API and prints the titles of
+the first 10 hot posts
+listed for a given subreddit
+"""
 import requests
 
 def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit
+
+    Args:
+        subreddit (str): The subreddit to search
+
+    Returns:
+        None
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
@@ -13,6 +28,13 @@ def top_ten(subreddit):
             for post in data['data']['children']:
                 print(post['data']['title'])
         else:
-            print(None)
+            print("None")
     else:
-        print(None)
+        print("None")
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])

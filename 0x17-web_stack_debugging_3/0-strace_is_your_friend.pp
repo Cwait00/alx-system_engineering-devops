9326afac78bf7ec5077b01ab90d0bdd690d52fd1
cwaita <cwaitanobongoza@gmail.com>
2024-03-06 11:06:58 -0800

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,20 @@
+# Puppet manifest to fix Apache 500 error
+
+# Ensure Apache package is installed
+package { 'apache2':
+  ensure => installed,
+}
+
+# Fix Apache configuration
+file { '/etc/apache2/apache2.conf':
+  ensure  => file,
+  content => template('apache2/apache2.conf.erb'),
+  notify  => Service['apache2'],
+}
+
+# Restart Apache service
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/apache2/apache2.conf'],
+}

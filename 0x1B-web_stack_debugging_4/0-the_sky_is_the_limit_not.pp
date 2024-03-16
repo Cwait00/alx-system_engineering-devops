fe904ef4cbf66e592b3d366fa22ab8a877b8e75c
cwaita <cwaitanobongoza@gmail.com>
2024-03-16 03:28:44 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,51 +1,13 @@
-# Contents of the Puppet manifest file used to fix the issue
-# Example Puppet manifest file for fixing Nginx configuration
+# Increases the amount of traffic an Nginx server can handle.
 
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => "
-    user www-data;
-    worker_processes auto;
-    pid /run/nginx.pid;
-
-    events {
-      worker_connections 1000; # Adjusted to handle 1000
-				#requests with 100 at a time
-      # multi_accept on;
-    }
-
-    http {
-
-      ##
-      # Basic Settings
-      ##
-
-      sendfile on;
-      tcp_nopush on;
-      tcp_nodelay on;
-      keepalive_timeout 65;
-      types_hash_max_size 2048;
-
-      server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-          try_files ${uri} ${uri}/ =404;
-        }
-      }
-    }
-  ",
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
 }
-
-# Reload Nginx to apply the changes
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File['/etc/nginx/nginx.conf'],
+#->
+# Restart Nginx
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
 }

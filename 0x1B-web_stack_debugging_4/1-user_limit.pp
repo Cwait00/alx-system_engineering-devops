faf4611a33f3990de9139a5f541a4b0051db76f9
cwaita <cwaitanobongoza@gmail.com>
2024-03-16 03:32:06 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,13 @@
-# Contents of the Puppet manifest file used to adjust the open
-file limit for the holberton user
-# Example Puppet manifest file for adjusting open file limit for a user
+# Enable the user holberton to login and open files without error.
 
-exec { 'increase_open_file_limit_for_holberton':
-  command => 'ulimit -n 4096',  # Adjust the open file limit as needed
-  user    => 'holberton',
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
